96dda6e7f61034228883a0fdafe2615d75c44ccc
Matheus Farias <m230888@dac.unicamp.br>
2023-03-25 17:25:34 -0300

    falta so a main

--- Labs/src/Main.java
+++ Labs/src/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Cliente client = new Cliente("Matheus", "432.130.688-45", "01/05/98", 24, "rua alan turing");
-        // System.out.println(client.toString());
-        // Cliente client2 = new Cliente("Matheus", "1234.-65", "01/05/98", 24, "rua alan turing");
+        Cliente client = new Cliente("Matheus", "432.130.688-45", "01/08/2003", 19, "Rua Saturnino de Brito");
 
-
-        System.out.println(client.validarCPF(client.getCpf()));
-        // System.out.println(client2.validarCPF(client2.getCpf()));
 
 
     }
